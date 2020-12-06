e1021ccb8cf19d422c94a577a0df73cf014057cd
Brian Pierson <14piersonb@gmail.com>
2020-12-06 01:31:03 -0500

    Renamed var and added comments to 6b for redability

--- Python/Day06/day6b.py
+++ Python/Day06/day6b.py
@@ -1,6 +1,6 @@
 content = open('input.txt', 'r').read().split("\n\n")
 sum = 0
 for group in content:
-    sets = [set(list(person)) for person in group.splitlines()]
-    sum += len(set.intersection(*sets))
+    listOfResponseSets = [set(list(singleResponse)) for singleResponse in group.splitlines()] #set(list(singleResponse)) gets a set of all the answers from one individual
+    sum += len(set.intersection(*listOfResponseSets)) #the intersection of all sets from a group will be only the elements they all share, aka all the questions everyone answered yes to. The len of that instersection is how many elements there are
 input(sum)
